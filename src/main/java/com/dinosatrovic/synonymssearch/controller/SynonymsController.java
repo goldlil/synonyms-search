c80bb53b8456e999e47196de6aaa73d089c8b475
Dino Šatrović <dino.satrovic@ws-mbp121-108.local>
2020-03-08 13:46:30 +0100

    added constraints validation and open-api contract

--- src/main/java/com/dinosatrovic/synonymssearch/controller/SynonymsController.java
+++ src/main/java/com/dinosatrovic/synonymssearch/controller/SynonymsController.java
@@ -1,17 +1,17 @@
 package com.dinosatrovic.synonymssearch.controller;
 
-import com.dinosatrovic.synonymssearch.controller.model.SynonymsInput;
+import com.dinosatrovic.synonymssearch.api.SynonymsSearchApi;
+import com.dinosatrovic.synonymssearch.model.SynonymsInput;
 import com.dinosatrovic.synonymssearch.service.SynonymsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("synonyms")
-public class SynonymsController {
+public class SynonymsController implements SynonymsSearchApi {
 
     private SynonymsService synonymsService;
 
@@ -20,22 +20,17 @@ public class SynonymsController {
         this.synonymsService = synonymsService;
     }
 
-    @PostMapping(value = "/add-collection", produces = "application/json")
-    public ResponseEntity<Void> addSynonyms(@RequestBody List<SynonymsInput> synonymsInput) {
-        synonymsService.addSynonyms(synonymsInput);
-        return new ResponseEntity<>(HttpStatus.CREATED);
-    }
-
-    @PostMapping(value = "/add", produces = "application/json")
-    public ResponseEntity<Void> addSynonyms(@RequestBody SynonymsInput synonymsInput) {
+    public ResponseEntity<Void> addSynonyms(SynonymsInput synonymsInput) {
         synonymsService.addSynonyms(synonymsInput.getWord(), synonymsInput.getSynonyms());
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
-
-    @GetMapping(value = "/search", produces = "application/json")
-    public ResponseEntity<List<String>> findSynonyms(@RequestParam(value = "word") String word, @RequestParam(value = "depth", required = false) Integer depth) {
-        return ResponseEntity.ok(synonymsService.searchSynonyms(word, depth));
+    public ResponseEntity<Void> addSynonymsCollection(List<SynonymsInput> synonymsInput) {
+        synonymsService.addSynonyms(synonymsInput);
+        return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    public ResponseEntity<List<String>> findSynonyms(String word, Integer depth) {
+        return ResponseEntity.ok(synonymsService.searchSynonyms(word, depth));
+    }
 }
